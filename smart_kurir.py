7d91fa30c682b6b68f73c44a28554c68bf1405b5
AurelJihanFahristy <ajihanfahristy@student.umrah.ac.id>
2025-03-26 21:19:01 +0700

    Update smart_kurir.py

--- smart_kurir.py
+++ smart_kurir.py
@@ -3,7 +3,7 @@ import numpy as np
 import heapq
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from scipy.spatial import distanc
+from scipy.spatial import distance
 
 def load_image(image_path):
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
